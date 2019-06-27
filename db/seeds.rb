d8ab05ff2fd0787f1b99cf362ca162bb88663984
Baylor Rae <baylor@thecodedeli.com>
2019-06-27 18:05:39 -0500

    add default rooms

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Room.create!([
+  {title: 'Cars'},
+  {title: 'Movies'},
+  {title: 'Books'},
+  {title: 'Food'},
+])
