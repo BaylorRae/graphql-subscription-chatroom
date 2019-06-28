e0491809cda192ee8571607f68b07f40e0396bbf
Baylor Rae <baylor@thecodedeli.com>
2019-06-28 07:42:26 -0500

    show oldest messages first

--- app/repositories/message_repository.rb
+++ app/repositories/message_repository.rb
@@ -9,7 +9,7 @@ class MessageRepository
   def latest_for_room(room_id:)
     Message
       .where(room_id: room_id)
-      .order(created_at: :desc)
+      .order(created_at: :asc)
       .limit(10)
   end
 end
